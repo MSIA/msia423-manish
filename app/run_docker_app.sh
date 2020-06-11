aec2295722171999479942c4623873092fd99226
manish2020-iitr <55149994+manish2020-iitr@users.noreply.github.com>
2020-06-10 22:01:30 -0500

    Update run_docker_app.sh

--- app/run_docker_app.sh
+++ app/run_docker_app.sh
@@ -2,8 +2,8 @@
 
 if [ $1 = 'add_db' ];
 then
-  winpty docker run -v "$(pwd)":"$(pwd)" -it --env aws_access_key_id --env aws_secret_access_key --env MYSQL_HOST --env MYSQL_PORT --env MYSQL_USER --env MYSQL_PASSWORD --env DATABASE_NAME -p 5000:5000 no_show app.py
+  winpty docker run -v "$(pwd)":"$(pwd)" -it --env aws_access_key_id --env aws_secret_access_key -p 5000:5000 no_show app.py
 elif [ $1 = 'predict' ];
 then
-  winpty docker run -v "$(pwd)":"$(pwd)" -it --env aws_access_key_id --env aws_secret_access_key --env MYSQL_HOST --env MYSQL_PORT --env MYSQL_USER --env MYSQL_PASSWORD --env DATABASE_NAME -p 5000:5000 no_show app_predict.py
+  winpty docker run -v "$(pwd)":"$(pwd)" -it --env aws_access_key_id --env aws_secret_access_key  -p 5000:5000 no_show app_predict.py
 fi
